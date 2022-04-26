4f66a1e74b08a2261cc10636046091d715f6bf66
Porrama Rakchaithanakorn <porrama.pee@gmail.com>
2022-04-26 21:33:14 +0700

    Update install_golang_wget.sh

--- install_golang_wget.sh
+++ install_golang_wget.sh
@@ -1,3 +1,3 @@
-    # this assumes your current version of Go is in the default location
-    wget https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz
-    tar -C /usr/local -zxvf go1.14.4.linux-amd64.tar.gz
+# this assumes your current version of Go is in the default location
+wget https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz
+tar -C /usr/local -zxvf go1.14.4.linux-amd64.tar.gz
