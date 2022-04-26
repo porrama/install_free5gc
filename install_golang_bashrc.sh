e2d081a50dad30fcb5e945c639ac84c8d52c7a13
Porrama Rakchaithanakorn <porrama.pee@gmail.com>
2022-04-26 22:11:06 +0700

    Delete install_golang_bashrc.sh

--- install_golang_bashrc.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p ~/go/{bin,pkg,src}
-# The following assume that your shell is bash
-echo 'export GOPATH=$HOME/go' >> ~/.bashrc
-echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
-echo 'export PATH=$PATH:$GOPATH/bin:$GOROOT/bin' >> ~/.bashrc
-echo 'export GO111MODULE=auto' >> ~/.bashrc
-source ~/.bashrc
